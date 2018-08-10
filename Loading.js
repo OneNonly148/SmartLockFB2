f0f0ada29ce102341c9d4dfffdc23556fdd70ec7
Iman Fitri <imanfitri@users.noreply.github.com>
2018-08-10 15:39:46 +0800

    Determining if a user is authenticated.

--- Loading.js
+++ Loading.js
@@ -2,7 +2,15 @@
 
 import React from 'react'
 import { View, Text, ActivityIndicator, StyleSheet } from 'react-native'
+import firebase from 'react-native-firebase'
 
+export default class Loading extends React.Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.props.navigation.navigate(user ? 'Main' : 'SignUp')
+    })
+  }
+}
 export default class Loading extends React.Component {
   render() {
     return (
