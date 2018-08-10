f65b0cb8a8a5ee0dd56da1109add384d5f43a93f
Iman Fitri <imanfitri@users.noreply.github.com>
2018-08-10 15:55:24 +0800

    3rd Troubleshooting

--- Loading.js
+++ Loading.js
@@ -4,7 +4,7 @@ import React from 'react'
 import { View, Text, ActivityIndicator, StyleSheet } from 'react-native'
 import firebase from 'react-native-firebase'
 
-export default class Loading extends React.Component {
+export class Loading extends React.Component {
   componentDidMount() {
     firebase.auth().onAuthStateChanged(user => {
       this.props.navigation.navigate(user ? 'Main' : 'SignUp')
