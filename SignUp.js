21d7983717b71bd714389a75ec2aedc1723aa6d9
Iman Fitri <imanfitri@users.noreply.github.com>
2018-08-10 15:41:06 +0800

    User prompt for signing up.

--- SignUp.js
+++ SignUp.js
@@ -2,6 +2,19 @@
 
 import React from 'react'
 import { StyleSheet, Text, TextInput, View, Button } from 'react-native'
+import firebase from 'react-native-firebase'
+
+export default class SignUp extends React.Component {
+  state = { email: '', password: '', errorMessage: null }
+
+  handleSignUp = () => {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(this.state.email, this.state.password)
+      .then(() => this.props.navigation.navigate('Main'))
+      .catch(error => this.setState({ errorMessage: error.message }))
+  }
+}
 
 export default class SignUp extends React.Component {
   state = { email: '', password: '', errorMessage: null }
