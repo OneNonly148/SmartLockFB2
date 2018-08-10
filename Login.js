a2ac2a7edb1a59dc3bcdd0b3d028066b7a0de820
Iman Fitri <imanfitri@users.noreply.github.com>
2018-08-10 15:51:45 +0800

    2nd Troubleshooting

--- Login.js
+++ Login.js
@@ -4,7 +4,7 @@ import React from 'react'
 import { StyleSheet, Text, TextInput, View, Button } from 'react-native'
 import firebase from 'react-native-firebase'
 
-export default class Login extends React.Component {
+export class Login extends React.Component {
   state = { email: '', password: '', errorMessage: null }
 
   handleLogin = () => {
