f27848817afc122bd2fbd1747a4ea8d917fe757d
Iman Fitri <imanfitri@users.noreply.github.com>
2018-08-10 16:15:19 +0800

    Adding firebase to the app

--- App.js
+++ App.js
@@ -2,6 +2,16 @@ import React from 'react'
 import { StyleSheet, Platform, Image, Text, View } from 'react-native'
 import { SwitchNavigator } from 'react-navigation'
 
+const firebaseConfig = {
+  apiKey: "AIzaSyB7yhJtk0ino5-hzZILOfx4fVEsws4qzPU",
+  authDomain: "smartlock-fc432.firebaseapp.com",
+  databaseURL: "https://smartlock-fc432.firebaseio.com",
+  projectId: "smartlock-fc432",
+  storageBucket: "smartlock-fc432.appspot.com",
+}
+
+firebase.initializeApp(firebaseConfig)
+
 // import the different screens
 import Loading from './Loading'
 import SignUp from './SignUp'
